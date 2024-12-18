06d770b34e86b4b9b64450acc96487a46e951f42
Yash Vardhan <yvardhan813@gmail.com>
2024-12-18 14:24:33 +0530

    Made the basic layout of the website

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+        <div>Hello World!!</div>
+      </body>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
