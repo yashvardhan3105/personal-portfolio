1fe1db19b763ee21e9d022ce765f2876bc4cc4bc
Yash Vardhan <yvardhan813@gmail.com>
2024-12-18 14:32:26 +0530

    Laid the basic foundation of the website.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,29 @@
+import Navbar from "../src/Components/Navbar/Navbar.jsx";
+import Header from "../src/Components/Header/Header.jsx";
+import Body from "../src/Components/Body/Body.jsx";
+import Footer from "../src/Components/Footer/Footer.jsx";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          <Header />
+        </div>
+      </header>
       <body>
-        <div>Hello World!!</div>
+        <div>
+          <Body />
+        </div>
       </body>
-      <footer></footer>
+      <footer>
+        <div>
+          <Footer />
+        </div>
+      </footer>
     </div>
   );
 }
